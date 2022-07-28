e7b13428b980952c2eb2bb78ff7ededd227260e3
Jimmy Hsiao <jimmy0921956909@gmail.com>
2022-07-28 11:43:41 +0800

    update: implement prepend turbo stream

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :room
 
-  broadcasts_to :room
-  # after_create_commit -> { broadcast_append_to room }
-  # after_destroy_commit -> { broadcast_remove_to room }
-  # after_update_commit -> { broadcast_update_to room }
+  # broadcasts_to :room
+  after_create_commit -> { broadcast_prepend_to room }
+  after_destroy_commit -> { broadcast_remove_to room }
+  after_update_commit -> { broadcast_update_to room }
 end
